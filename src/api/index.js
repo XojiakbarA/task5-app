293431c21c74a39231ab1fa2154e9485c187ca15
Xojiakbar <87039534+XojiakbarA@users.noreply.github.com>
2022-08-06 08:00:57 +0500

    edited base url for deployment

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "http://localhost:8080/api/"
+export const BASE_URL = "https://taskfiveapi.herokuapp.com/api/"
 
 const instance = axios.create({
     baseURL: BASE_URL,
